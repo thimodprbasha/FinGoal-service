3b2290c50e1e4eb349fd98b46c5fc51bc7ba3af5
thimo <thimodprabsha@icloud.con>
2024-02-06 16:11:15 +0530

    setup done

---
+++ src/main/java/com/example/fingoal/FinGoalApplication.java
@@ -0,0 +1,13 @@
+package com.example.fingoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinGoalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinGoalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fingoal/FinGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fingoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinGoalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
