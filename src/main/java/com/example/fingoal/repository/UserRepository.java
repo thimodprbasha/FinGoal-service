0f5c7a181ec814a5b6a246a97b3505d3ceac9733
thimo <thimodprabsha@icloud.con>
2024-02-12 18:02:48 +0530

    added banner

--- src/main/java/com/example/fingoal/repository/UserRepository.java
+++ src/main/java/com/example/fingoal/repository/UserRepository.java
@@ -3,11 +3,10 @@ package com.example.fingoal.repository;
 import com.example.fingoal.model.Role;
 import com.example.fingoal.model.User;
 import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User , Long> , PagingAndSortingRepository<User , Long>{
